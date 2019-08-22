a3cbe60f22ca150e9683fef8669929422db9f14a
胡旷杰 <zjxshukj@gmail.com>
2019-08-22 11:10:19 +0800

    初始化，添加css reset

---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+        height
+</style>
