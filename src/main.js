874bdb6e9100cc91e2870090111676d54581b5a8
胡旷杰 <zjxshukj@gmail.com>
2019-08-24 11:24:05 +0800

    fix es6 bug

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'babel-polyfill'   //有些手机浏览器不支持ES6，添加这个可以兼容
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
